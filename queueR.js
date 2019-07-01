15896012d602502804a9482cd695427c54d061e7
Justin Filip <justin.filip@outlook.com>
2019-07-01 13:49:31 -0500

    Add files via upload
    To use, open Developer Tools in Chrome or Firefox, click the console tab, paste the code and hit enter.

---
+++ queueR.js
@@ -0,0 +1,12 @@
+var iframe = document.getElementById('ext-comp-1005');
+
+var x = iframe.contentDocument.getElementsByClassName("refreshListButton")[0]. id;
+
+function qRefresh() {
+	iframe.contentDocument.getElementById(x).dispatchEvent(new MouseEvent("click"));
+}
+
+qRefresh();
+
+setInterval(function() {
+	qRefresh()}, 30000)
