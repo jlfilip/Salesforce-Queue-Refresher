49e8240e55cbdd579d9bef693b0ea670d8509c50
Justin Filip <justin.filip@outlook.com>
2019-07-01 13:51:06 -0500

    Update queueR.js

--- queueR.js
+++ queueR.js
@@ -1,3 +1,5 @@
+//To use, copy the code below and paste it in the 'Console' tab of Developer Tools in Chrome or Firefox.
+
 var iframe = document.getElementById('ext-comp-1005');
 
 var x = iframe.contentDocument.getElementsByClassName("refreshListButton")[0]. id;
