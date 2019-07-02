83178f5527b914848d3dd552fe6ee3d6c0ac71d6
Justin Filip <justin.filip@outlook.com>
2019-07-02 09:04:52 -0500

    Update queueR.js

--- queueR.js
+++ queueR.js
@@ -2,7 +2,7 @@
 
 var iframe = document.getElementById('ext-comp-1005');
 
-var x = iframe.contentDocument.getElementsByClassName("refreshListButton")[0]. id;
+var x = iframe.contentDocument.getElementsByClassName("refreshListButton")[0].id;
 
 function qRefresh() {
 	iframe.contentDocument.getElementById(x).dispatchEvent(new MouseEvent("click"));
